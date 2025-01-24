e8c2e2747e1c1a2bbc189919c570970bc660e323
MalikT <maliktremain1@gmail.com>
2025-01-24 06:24:00 +0000

    Found issue with array iterators

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 
+#include "cll/array.hpp"
+
+template<typename T>
+void prnt(T const& val)
+{
+    for (auto const& x : val)
+        std::cout << x << '\t';
+    std::cout << '\n';
+}
 int main()
 {
+    cll::Array<int, 4> arr(92);
+    prnt(arr);
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
